25c452927e90850f9537f26d5f20aeea9246ac92
Pablo Quiceno <jpquiceno@kometsales.com>
2019-07-01 17:24:53 -0500

    Se avanza en CRUD con graphql

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const {graphqlExpress ,graphiqlExpress} = require('graphql-server-express')
+const { makeExecutableSchema } = require('graphql-tools')
+const { merge } = require('lodash')
+
+const courseTypesDef = require('./types/course.types')
+const courseResolvers = require('./resolvers/course.resolvers')
+
+mongoose.connect('mongodb://localhost/graphql_db_course', {useNewUrlParser : true})
+
+const app = express()
+
+const typeDefs = `
+type Alert{
+  message: String
+}
+type Query{
+  _ : Boolean
+}
+type Mutation {
+  _ : Boolean
+}
+`
+
+const resolvers = {}
+
+const schema = makeExecutableSchema({
+  typeDefs: [typeDefs, courseTypesDef],
+  resolvers: merge(resolvers, courseResolvers)
+})
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema
+}))
+
+app.use('/graphiql', graphiqlExpress({ endpointURL: 'graphql' }))
+
+app.listen(3000, () => console.log(`Sever running on http://localhost:3000`))
+
